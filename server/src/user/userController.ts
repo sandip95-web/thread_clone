f881f7450d5c2607ce10ba094b2842b5d094edfa
sandip95-web <sandipmaharjan95@gmail.com>
2024-11-24 16:12:57 +0545

    add login controller and route

--- server/src/user/userController.ts
+++ server/src/user/userController.ts
@@ -2,16 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { User } from "./userType";
 import { config } from "../config/config";
 
 const generateToken = async (res: Response, user: User, next: NextFunction) => {
   try {
-    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    const accessToken = sign({ sub: user._id }, config.jwtSecret as string, {
       expiresIn: config.jwtExpire,
     });
-    return token;
+    res.cookie("token", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 10,
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
   } catch (error) {
     return next(createHttpError(500, "Error while signing token"));
   }
@@ -24,19 +29,23 @@ export const signIn = async (
 ) => {
   try {
     const { username, email, password } = req.body;
+    //validating if the field are empty or not
     if (!username || !email || !password) {
       return next(
         createHttpError(400, "Username, email, and password are required.")
       );
     }
+    //checking if user exist or not
     const userExist = await userModel.findOne({ email });
     if (userExist) {
       return next(createHttpError(409, "User already exists with this email."));
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     if (!hashedPassword) {
       return next(createHttpError(400, "Error while hashing password"));
     }
+    //creating new user
     const newUser = await userModel.create({
       username,
       email,
@@ -45,24 +54,48 @@ export const signIn = async (
     if (!newUser) {
       return next(createHttpError(400, "Error while saving user."));
     }
-    const accessToken = await generateToken(res, newUser, next);
-    res.cookie("token", accessToken, {
-      maxAge: 1000 * 60 * 60 * 24 * 10,
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    });
+    //generate Token
+    await generateToken(res, newUser, next);
+
     res.status(201).json({
       success: true,
       message: `User Sign in successfully! Welcome ${newUser?.username}`,
     });
   } catch (error) {
-    console.log("Error:",error)
     return next(
       createHttpError(
         500,
         "An error occurred while processing your request. Please try again later."
       )
     );
+  }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(createHttpError(400, "Email and Password are required."));
+    }
+    const findUser = await userModel.findOne({ email }).select("+password");
+    if (!findUser) {
+      return next(createHttpError(400, "User with that email doesn't exist"));
+    }
+
+    const checkPassword = await bcrypt.compare(password, findUser.password);
+    if (!checkPassword) {
+      return next(createHttpError(400, "Incorrect email or password."));
+    }
+    await generateToken(res, findUser, next);
+    res.status(200).json({
+      success: true,
+      message: "User login successfully",
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while login in."));
   }
 };
--- server/src/user/userRoute.ts
+++ server/src/user/userRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signIn } from './userController';
+import { loginUser, signIn } from './userController';
 
 const userRouter=express.Router();
 
 
 userRouter.post("/signin",signIn)
+userRouter.post("/login",loginUser)
 
 export default userRouter;
